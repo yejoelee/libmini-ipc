4f233a4a453c620593beb41a767efd094f33b078
yejoe <yejoe.lee@gmail.com>
2021-12-25 15:29:32 +0800

    update samples/ipclib_send.c

--- samples/ipclib_send.c
+++ samples/ipclib_send.c
@@ -33,10 +33,19 @@ int main(int argc, char *argv[])
 	ret = ipc_init("send", app_msg_handler);
 	if (ret < 0)
 		err_exit("ipc_init error\n");
-    
+
+	if(argc < 4) {
+		printf("Usage:%s <app_name> <source> <content>\n", argv[0]);
+		printf("Example:%s mango %s \"I am %s.\"\n", argv[0], argv[0], argv[0]);
+		return -1;
+	}
+
     char *app_name = argv[1];
 
 	struct ipc_msg msg = {0};
+	msg.type = 1;
+	memcpy(msg.source, argv[2], strlen(argv[2]));
+	memcpy(msg.content, argv[3], strlen(argv[3]));
 
     ret = ipc_send_msg_async(app_name, &msg);
     if(ret) printf("ipc_send_msg_async fail\n");
